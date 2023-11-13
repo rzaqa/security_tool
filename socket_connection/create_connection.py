e488f92a16dff14cf263c496347ea52bb7139431
Rail Zakirov <rzakirov@purestorage.com>
2023-11-13 16:44:32 +0100

    Lesson 1 - create a socket connection

---
+++ socket_connection/create_connection.py
@@ -0,0 +1,25 @@
+import socket
+
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = 'localhost'
+    # host = '8.8.8.8'
+    port = 11000
+    # try:
+    #     s.connect((host, port))
+    #     print('it works')
+    # except Exception as e:
+    #     print(e)
+    #     print(e.args)
+    #     print(e.__cause__)
+
+    # second example how to create s socket connection
+    result = s.connect_ex((host, port))
+    print('Result is {}'.format(result))   # will show '0' if connection was successfull
+
+    s.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ socket_connection/utils.py
@@ -0,0 +1,16 @@
+from timeit import default_timer as timer
+
+
+def timefunc(func):
+    def inner(*args, **kwargs):
+        start = timer()
+        results = func(*args, **kwargs)
+        end = timer()
+        message = '{} took {} seconds'.format(func.__name, end - start)
+        print(message)
+        return results
+    return inner
+
+
+  
+
